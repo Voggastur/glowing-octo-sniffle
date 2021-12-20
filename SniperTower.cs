f8d66bbc0f8396af5595f7997414cab00be342ac
Johan Kjellin <v3x0r_x014@hotmail.com>
2021-12-20 19:37:19 +0100

    improve accuracy for SniperTower (100%)

--- SniperTower.cs
+++ SniperTower.cs
@@ -4,7 +4,7 @@ namespace TreehouseDefense
     class SniperTower : Tower
     {
         protected override int Range { get; } = 2;
-        protected override double Accuracy { get; } = .95;
+        protected override double Accuracy { get; } = 1;
 
         public SniperTower(MapLocation location) : base(location)
         {
